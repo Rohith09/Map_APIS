39fb961064da6419b51eb1f45358db8147d84579
Rohith09 <kandirohith09@gmail.com>
2018-03-02 23:14:38 +0530

    Minor Coding.

--- app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
+++ app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.chandrassekhar.map_apis;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (googleServicesAvailability()){
+            Toast.makeText(this,"Perfect!!!",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean googleServicesAvailability(){
+        GoogleApiAvailability api=GoogleApiAvailability.getInstance();
+        int isAvailable=api.isGooglePlayServicesAvailable(this); //it returns three values.
+        if (isAvailable == ConnectionResult.SUCCESS){
+            return true;
+        }
+
+        else if (api.isUserResolvableError(isAvailable)){   //if the error is fixable then it returns a dialogue box.
+            Dialog dialog =api.getErrorDialog(this,isAvailable,0);  // (context,errorcode(int),requestcode (int)).
+            dialog.show();
+        }
+
+        else{
+            Toast.makeText(this,"Cannot Connect to play Sevices",Toast.LENGTH_LONG).show();
+        }
+        return false;
     }
 }
