9255c9205ae08cb9a02835bcfd6650bcee03b962
Rohith09 <kandirohith09@gmail.com>
2018-03-02 23:39:19 +0530

    Added code to Zoom into a location on google maps.

--- app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
+++ app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
@@ -7,33 +7,74 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mgoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         if (googleServicesAvailability()){
             Toast.makeText(this,"Perfect!!!",Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+
         }
+        else{
+            Toast.makeText(this," No Google Maps",Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
-    public boolean googleServicesAvailability(){
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean googleServicesAvailability(){  //user created function in order to check whether the google play store services are available on the users phone or not.
         GoogleApiAvailability api=GoogleApiAvailability.getInstance();
         int isAvailable=api.isGooglePlayServicesAvailable(this); //it returns three values.
-        if (isAvailable == ConnectionResult.SUCCESS){
+        if (isAvailable == ConnectionResult.SUCCESS){   // in case of Availability of google play services.
             return true;
         }
 
-        else if (api.isUserResolvableError(isAvailable)){   //if the error is fixable then it returns a dialogue box.
+        else if (api.isUserResolvableError(isAvailable)){   //if the error is fixable then it returns a dialogue box , maybe updating  a version. etc
             Dialog dialog =api.getErrorDialog(this,isAvailable,0);  // (context,errorcode(int),requestcode (int)).
             dialog.show();
         }
 
-        else{
+        else{   //if the google play store services are not available on the users phone.
             Toast.makeText(this,"Cannot Connect to play Sevices",Toast.LENGTH_LONG).show();
         }
         return false;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mgoogleMap = googleMap;
+        goToLocationZoom(28.4985897,77.3759081, 15);
+
+    }
+
+    private void goToLocation(double lat, double lng) {
+        LatLng ll = new LatLng(lat , lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mgoogleMap.moveCamera(update);  //takes this to a particular location.
+
+    }
+
+    private void goToLocationZoom(double lat, double lng,float  z) {
+        LatLng ll = new LatLng(lat , lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll,z);
+        mgoogleMap.moveCamera(update);  //takes this to a particular location.
+
     }
 }
