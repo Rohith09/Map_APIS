cc5be0ab21fb23d8ea9b8a57f041730e06f6fc81
Rohith09 <kandirohith09@gmail.com>
2018-03-09 17:43:29 +0530

    Created a xml file (Menu.xml) and added it on the main layout.

--- app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
+++ app/src/main/java/com/example/chandrassekhar/map_apis/MainActivity.java
@@ -5,6 +5,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -104,5 +105,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             e.printStackTrace();
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);  //Provides a menu on top right corner which shows a list of map types.
+        return super.onCreateOptionsMenu(menu);
     }
 }
